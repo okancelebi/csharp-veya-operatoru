723298791bd223126e714b6ac20c03cd9ae6fce0
Okan Çelebi <okancelebi06@gmail.com>
2023-02-24 22:37:41 +0300

    Add files via upload

---
+++ csharp veya operatoru/csharp veya operatoru/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_veya_operatoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Veya Operatörü : Altgr+ z'nin yanındaki tuş ile yapılır.
+            int sayi1 = 10;
+            int sayi2 = 10;
+            int sayi3 = 4;
+            bool kontrolet = sayi1 == sayi2 || sayi1 < sayi3;
+            Console.WriteLine(kontrolet);
+            Console.ReadLine();
+        }
+    }
+}
